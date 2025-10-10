74c66b3ac5f765259c9d12cfc7b0b65305ba07c9
JasonCho1112001 <jcho111@ucsc.edu>
2025-10-10 02:05:39 +0000

    This is our commit
    Co-authored-by: Mani4141 <Mani4141@users.noreply.github.com>
    Co-authored-by: jaredNussbaum <jaredNussbaum@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+console.log("This is Jared's edit");
+console.log("This is Manvir");
+console.log("This is Jason's Edit");
